92d48b02c8d5a0d10ca11da4f20d4dfc899e72d9
Ryeo345 <philipruan345@gmail.com>
2023-05-15 13:21:58 -0400

    successfully loading info window data in the google maps and linked the markers to the listing details pages

--- src/Components/MapData.js
+++ src/Components/MapData.js
@@ -1,34 +1,61 @@
-import React, { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import React, { useMemo, useState } from "react";
+import { GoogleMap, useLoadScript, Marker , InfoWindow} from "@react-google-maps/api";
 import secrets from "../../secrets";
 import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom'
 
 const MapData = () => {
+  const { listings } = useSelector((state) => state);
+  const [activeMarker, setActiveMarker] = useState(null);
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: secrets.API_KEY,
   });
-  if (!isLoaded) return <div>Loading...</div>;
-  return <Map />;
-};
-
-function Map() {
-  const { listings } = useSelector((state) => state);
+  const handleMarkerClick = (marker) => {
+    setActiveMarker(marker);
+  };
+  const handleInfoWindowClose = () => {
+    setActiveMarker(null);
+  };
 
   const center = useMemo(
-      () => ({ lat: 40.697679968368114, lng: -74.03137868378906 }),
-      []
+    () => ({ lat: 40.697679968368114, lng: -74.03137868378906 }),
+    []
   );
+
+  if (!isLoaded) return <div>Loading...</div>;
+
   return (
-      <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-        <Marker position={center} />
-        {listings.map((listing) => (
-          <Marker
-                key={listing.id}
-                position={{ lat: parseFloat(listing.lat), lng: parseFloat(listing.long) }}
-            />
-        ))}
-      </GoogleMap>
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+      <Marker position={center} />
+      {listings.map((listing) => (
+        <Marker
+          key={listing.id}
+          position={{
+            lat: parseFloat(listing.lat),
+            lng: parseFloat(listing.long),
+          }}
+          title={listing.storage_type}
+          onClick={() => {
+            handleMarkerClick(listing);
+          }}
+        />
+      ))}
+      {activeMarker && (
+          <InfoWindow
+              position={{ lat: parseFloat(activeMarker.lat), lng: parseFloat(activeMarker.long) }}
+              onCloseClick={handleInfoWindowClose}
+          >
+            <div>
+              <Link to={`/listings/${activeMarker.id}`}>
+                <img className='infoWindow-img' src={activeMarker.photo} />
+                <span><b>{activeMarker.length}X{activeMarker.width}</b> {activeMarker.storage_type} </span>
+                <span>{activeMarker.city}, {activeMarker.state}</span>
+              </Link>
+            </div>
+          </InfoWindow>
+      )}
+    </GoogleMap>
   );
-}
+};
 
 export default MapData;
--- src/Components/Rentals.js
+++ src/Components/Rentals.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import MapData from "./MapData";
+import { Link } from 'react-router-dom'
 
 const Rentals = () => {
     const { listings } = useSelector(state => state);
     return (
-        <div>
+        <div id="page-layout">
             <MapData />
-            <div>
+            <div id='content-container'>
                 {
                     listings.map(listing => {
                         return (
-                            <div key={listing.id}>
-                                <p>{listing.name}</p>
-                                <img src={listing.photo} />
+                            <div key={listing.id} className='listing-container'>
+                                <div className='listing-img-container'>
+                                    <Link to={`/listings/${listing.id}`}>
+                                        <img className='listing-img' src={listing.photo} />
+                                    </Link>
+                                </div>
+                                <div className='listing-info-container'>
+                                    <Link to={`/listings/${listing.id}`}>
+                                        {listing.length} X {listing.width}
+                                    </Link>
+                                </div>
                             </div>
                         )
                     })
