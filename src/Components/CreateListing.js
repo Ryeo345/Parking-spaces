50948380f67f76273706c11b9800f171bcb58565
Ryeo345 <philipruan345@gmail.com>
2023-08-31 13:11:50 -0400

    added CSS

--- src/Components/CreateListing.js
+++ src/Components/CreateListing.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { createListing } from "../store/listings";
 import { TextField, Button, Input } from "@mui/material";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const CreateListing = () => {
   const { auth } = useSelector((state) => state);
@@ -37,114 +37,136 @@ const CreateListing = () => {
     ev.preventDefault();
     const { id } = auth;
     await dispatch(
-      createListing({ userId: id, street, city, state, country, zipCode , photo, name, width, length, summary, storage_type, monthly_price})
+      createListing({
+        userId: id,
+        street,
+        city,
+        state,
+        country,
+        zipCode,
+        photo,
+        name,
+        width,
+        length,
+        summary,
+        storage_type,
+        monthly_price,
+      })
     );
-    navigate('/');
+    navigate("/");
   };
 
   return (
-      <div className="creatingList">
-        <div>
-          <h1>Post a new Spot</h1></div>
-        <div className="creatingList-form-container">
-          <form onSubmit={_createListing}>
-            <TextField
-                required
-                label="name"
-                variant="outlined"
-                value={name}
-                onChange={(ev) => setName(ev.target.value)}
-            />
-            <TextField
-                required
-                label="summary"
-                variant="outlined"
-                value={summary}
-                onChange={(ev) => setSummary(ev.target.value)}
-            />
-            <TextField
-                required
-                label="length"
-                variant="outlined"
-                value={length}
-                onChange={(ev) => setLength(ev.target.value === "" ? 0 : parseInt(ev.target.value))}
-            />
-            <TextField
-                required
-                label="width"
-                variant="outlined"
-                value={width}
-                onChange={(ev) => setWidth(ev.target.value === "" ? 0 : parseInt(ev.target.value))}
-            />
-            <TextField
-                required
-                label="monthly price"
-                variant="outlined"
-                value={monthly_price}
-                onChange={(ev) => setMonthlyPrice(ev.target.value === "" ? 0 : parseInt(ev.target.value))}
-            />
-            <TextField
-                required
-                label="storage type"
-                variant="outlined"
-                value={storage_type}
-                onChange={(ev) => setStorageType(ev.target.value)}
-            />
-            <Input type="file" ref={ref} />
-            {!!photo && (
-                <img
-                    src={photo}
-                    style={{
-                      width: "100px",
-                      boxShadow: "2px 2px 4px rgba(0, 0, 0, 0.4)",
-                    }}
-                />
-            )}
-            <TextField
-                required
-                label="Street Address"
-                variant="outlined"
-                value={street}
-                onChange={(ev) => setStreet(ev.target.value)}
-            />
-            <TextField
-                required
-                label="City"
-                variant="outlined"
-                value={city}
-                onChange={(ev) => setCity(ev.target.value)}
-            />
-            <TextField
-                required
-                label="State"
-                variant="outlined"
-                value={state}
-                onChange={(ev) => setState(ev.target.value)}
-            />
-            <TextField
-                required
-                label="Country"
-                variant="outlined"
-                value={country}
-                onChange={(ev) => setCountry(ev.target.value)}
-            />
-            <TextField
-                required
-                label="Zip Code"
-                variant="outlined"
-                value={zipCode}
-                onChange={(ev) => setZipCode(ev.target.value)}
-            />
-            <Button
-                type="submit"
-                variant="contained"
-                disabled={!street || !city || !state || !country || !zipCode}
-            >
-              Create
-            </Button>
-          </form>
+    <div className="creatingList">
+      <div>
+        <h1>Post a new Spot</h1>
       </div>
-
+      <div className="creatingList-form-container">
+        <form onSubmit={_createListing}>
+          <TextField
+            required
+            label="name"
+            variant="outlined"
+            value={name}
+            onChange={(ev) => setName(ev.target.value)}
+          />
+          <TextField
+            required
+            label="summary"
+            variant="outlined"
+            value={summary}
+            onChange={(ev) => setSummary(ev.target.value)}
+          />
+          <TextField
+            required
+            label="length"
+            variant="outlined"
+            value={length}
+            onChange={(ev) =>
+              setLength(ev.target.value === "" ? 0 : parseInt(ev.target.value))
+            }
+          />
+          <TextField
+            required
+            label="width"
+            variant="outlined"
+            value={width}
+            onChange={(ev) =>
+              setWidth(ev.target.value === "" ? 0 : parseInt(ev.target.value))
+            }
+          />
+          <TextField
+            required
+            label="monthly price"
+            variant="outlined"
+            value={monthly_price}
+            onChange={(ev) =>
+              setMonthlyPrice(
+                ev.target.value === "" ? 0 : parseInt(ev.target.value)
+              )
+            }
+          />
+          <TextField
+            required
+            label="storage type"
+            variant="outlined"
+            value={storage_type}
+            onChange={(ev) => setStorageType(ev.target.value)}
+          />
+          <Input type="file" ref={ref} />
+          {!!photo && (
+            <img
+              src={photo}
+              style={{
+                width: "100px",
+                boxShadow: "2px 2px 4px rgba(0, 0, 0, 0.4)",
+              }}
+            />
+          )}
+          <TextField
+            required
+            label="Street Address"
+            variant="outlined"
+            value={street}
+            onChange={(ev) => setStreet(ev.target.value)}
+          />
+          <TextField
+            required
+            label="City"
+            variant="outlined"
+            value={city}
+            onChange={(ev) => setCity(ev.target.value)}
+          />
+          <TextField
+            required
+            label="State"
+            variant="outlined"
+            value={state}
+            onChange={(ev) => setState(ev.target.value)}
+          />
+          <TextField
+            required
+            label="Country"
+            variant="outlined"
+            value={country}
+            onChange={(ev) => setCountry(ev.target.value)}
+          />
+          <TextField
+            required
+            label="Zip Code"
+            variant="outlined"
+            value={zipCode}
+            onChange={(ev) => setZipCode(ev.target.value)}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            disabled={!street || !city || !state || !country || !zipCode}
+          >
+            Create
+          </Button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/Components/IndividualListing.js
+++ src/Components/IndividualListing.js
@@ -35,53 +35,53 @@ const IndividualListing = () => {
     return null;
   }
   return (
-    <div className="listing-details-container">
-      <div className="listing-details-info">
-        <div>
-          <span>{listing.bookingStatus} for ${listing.monthly_price} per month</span>
+      <div className="listing-details-container">
+        <div className="listing-details-info">
+          <div className= "listing-status">
+            <span>{listing.bookingStatus} for ${listing.monthly_price} per month</span>
+          </div>
+          <div className="listing-details-img">
+            <img src={listing.photo} />
+          </div>
+          <div className="listing-details-dimensions">
+            <span>
+              {listing.length} X {listing.width} {listing.storage_type}
+            </span>
+          </div>
+          <div className="listing-details-city-state">
+            <span>
+              {listing.city}, {listing.state}
+            </span>
+          </div>{" "}
+          <div className="listing-details-summary">
+            <span>{listing.summary}</span>
+          </div>{" "}
+          <div className= "listing-status">
+            <span>{listing.bookingStatus} for ${listing.monthly_price} per month</span>
+          </div>
+          <div>
+            {listing.bookingStatus === "OCCUPIED" ? (
+              <span>{listing.expiry_date}</span>
+            ) : null}
+          </div>
         </div>
-        <div className="listing-details-img">
-          <img src={listing.photo} />
-        </div>
-        <div className="listing-details-dimensions">
-          <span>
-            {listing.length} X {listing.width} {listing.storage_type}
-          </span>
-        </div>
-        <div className="listing-details-city-state">
-          <span>
-            {listing.city}, {listing.state}
-          </span>
-        </div>{" "}
-        <div className="listing-details-summary">
-          <span>{listing.summary}</span>
-        </div>{" "}
-        <div>
-          <span>{listing.bookingStatus} for ${listing.monthly_price} per month</span>
-        </div>
-        <div>
-          {listing.bookingStatus === "OCCUPIED" ? (
-            <span>{listing.expiry_date}</span>
-          ) : null}
+        <div className="listing-details-form">
+            {listing.bookingStatus === "AVAILABLE" &&
+            listing.userId !== auth.id ? (
+              <form onSubmit={rentSpot}>
+                <DatePicker
+                  label="Expiry Date"
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+                <Button type="submit" variant="contained">
+                  Rent Now
+                </Button>
+              </form>
+            ) : null}
         </div>
       </div>
-      <div className="listing-details-form">
-          {listing.bookingStatus === "AVAILABLE" &&
-          listing.userId !== auth.id ? (
-            <form onSubmit={rentSpot}>
-              <DatePicker
-                label="Expiry Date"
-                value={selectedDate}
-                onChange={handleDateChange}
-                renderInput={(params) => <TextField {...params} />}
-              />
-              <Button type="submit" variant="contained">
-                Rent Now
-              </Button>
-            </form>
-          ) : null}
-      </div>
-    </div>
   );
 };
 
